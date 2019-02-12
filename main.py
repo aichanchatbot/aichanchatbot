8701fce7f26bf84eeb320ed186c2005d4cd82fb8
aichanchatbot <47549695+aichanchatbot@users.noreply.github.com>
2019-02-12 20:10:55 +0900

    オウム返し
    オウム返し

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+ 
+REPLY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
+text = "こんにちは"
+
+def post_text(reply_token, text):
+    header = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer {LINE_BOT_CHANNEL_TOKEN}"
+    }
+    payload = {
+          "replyToken":reply_token,
+          "messages":[
+                {
+                    "type":"text",
+                    "text": text
+                }
+            ]
+    }
+    requests.post(REPLY_ENDPOINT, headers=header, data=json.dumps(payload))
