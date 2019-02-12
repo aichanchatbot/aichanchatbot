36306d0784292571d10efb4cb65c7e136d46fb79
aichanchatbot <47549695+aichanchatbot@users.noreply.github.com>
2019-02-13 01:33:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,19 @@
-from datetime import datetime
-from flask import jsonify
-from MadproBot import app
+import requests
+ 
+REPLY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
 
-@app.route('/')
-@app.route('/home')
-def home():
-    return jsonify({
-        "message": "this endpoint is active"
-    })
-
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    reply_to_line(request.json)
-    return '', 200, {}
-
-def reply_to_line(body):
-    for event in body['events']:
-        responses = []
-
-        replyToken = event['replyToken']
-        type = event['type']
-        
-        if type == 'message':
-            message = event['message']
-            
-            if message['type'] == 'text':
-                # そのままオウム返し
-                responses.append(LineReplyMessage.make_text_response(message['text']))
-            else:
-                # テキスト以外のメッセージにはてへぺろしておく
-                responses.append(LineReplyMessage.make_text_response('てへぺろ'))
-
-        # 返信する
-        LineReplyMessage.send_reply(replyToken, responses)
-
-
-class LineReplyMessage:
-    ReplyEndpoint = 'https://api.line.me/v2/bot/message/reply'
-    AccessToken = '<LINE_CHANNEL_ACCESS_TOKEN>'
-
-    @staticmethod
-    def make_text_response(text):
-        return {
-            'type': 'text',
-            'text': text
-        }
-
-    @staticmethod
-    def send_reply(replyToken, messages):
-        reply = {
-            'replyToken': replyToken,
-            'messages': messages
-        }
-
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer {}'.format(LineReplyMessage.AccessToken)
-        }
-
-        requests.post(
-            LineReplyMessage.ReplyEndpoint,
-            data=json.dumps(reply),
-            headers=headers)
+def post_text(reply_token, text):
+    header = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer" "<tkRCRSMlOvPMRWBmcARN3isuPn0uOwUfi8BYH2369KNNNAPMJuu2i0KmTHr7HnC609rktbNOBXk9ZeUz1c9KNTrLNPVFSJg8C+4Os4d5n+oklTOXPrqkDuG+dsMzWKIRplvCuid+eAwpXNOZMDSXbgdB04t89/1O/w1cDnyilFU=>"
+    }
+    payload = {
+          "replyToken":reply_token,
+          "messages":[
+                {
+                    "type": "text",
+                    "text": "こんにちは"
+                }
+            ]
+    }
+    requests.post(REPLY_ENDPOINT, headers=header, data=json.dumps(payload))
