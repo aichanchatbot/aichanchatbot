92747388452ee3ea2f3cf9106fc84f856bef19be
aichanchatbot <47549695+aichanchatbot@users.noreply.github.com>
2019-02-12 20:43:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ REPLY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
 
 def post_text(reply_token, text):
     header = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer {tMCU/hu7j4Pq0ePqkb2cqytGQkjgK0Gh3y6a4viauHuYzE+ZTHvQDJRZc4NcFjhlw/VHwvNiYGPdkLMoRqFbv/IrXVAQAUZcq4o9Ki98eu7mfNYEUsRMIfK7Vwy2TWDk9z5lFqkihrMH4cOGlIKImgdB04t89/1O/w1cDnyilFU=}"
+        "Content-Type":"application/json",
+        "Authorization":"Bearer{ACCESS_TOKEN}"
     }
     payload = {
           "replyToken":reply_token,
           "messages":[
                 {
                     "type":"text",
-                    "text": text
+                    "text":text
                 }
             ]
     }
