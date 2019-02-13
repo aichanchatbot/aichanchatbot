3efdf81f123d3044334ba192c90e428b1c78105d
aichanchatbot <47549695+aichanchatbot@users.noreply.github.com>
2019-02-13 11:52:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-import requests
- 
-REPLY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
+import base64
+import hashlib
+import hmac
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+from linebot.exceptions import LineBotApiError
 
-def post_text(reply_token, text):
-    header = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer" "<tkRCRSMlOvPMRWBmcARN3isuPn0uOwUfi8BYH2369KNNNAPMJuu2i0KmTHr7HnC609rktbNOBXk9ZeUz1c9KNTrLNPVFSJg8C+4Os4d5n+oklTOXPrqkDuG+dsMzWKIRplvCuid+eAwpXNOZMDSXbgdB04t89/1O/w1cDnyilFU=>"
-    }
-    payload = {
-          "replyToken":reply_token,
-          "messages":[
-                {
-                    "type": "text",
-                    "text": "こんにちは"
-                }
-            ]
-    }
-    requests.post(REPLY_ENDPOINT, headers=header, data=json.dumps(payload))
+channel_secret = "806f6cc5d80d5899bc1a8c293f35603c"
+body = ""
+hash = hmac.new(channel_secret.encode('utf-8'),
+    body.encode('utf-8'), hashlib.sha256).digest()
+signature = base64.b64encode(hash)
+# Compare X-Line-Signature request header and the signature
+
+line_bot_api = LineBotApi('<channel access token>')
+
+try:
+    line_bot_api.reply_message('<reply_token>', TextSendMessage(text='Hello World!'))
+except LineBotApiError as e:
+    print("error")
+    # error handle
