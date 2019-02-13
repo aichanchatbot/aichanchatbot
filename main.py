5457d228812d3480832ce66af2b2500ef6e4c23d
aichanchatbot <47549695+aichanchatbot@users.noreply.github.com>
2019-02-13 11:58:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,45 @@
-import base64
-import hashlib
-import hmac
-from linebot import LineBotApi
-from linebot.models import TextSendMessage
-from linebot.exceptions import LineBotApiError
+from flask import Flask, request, abort
 
-channel_secret = "806f6cc5d80d5899bc1a8c293f35603c"
-body = ""
-hash = hmac.new(channel_secret.encode('utf-8'),
-    body.encode('utf-8'), hashlib.sha256).digest()
-signature = base64.b64encode(hash)
-# Compare X-Line-Signature request header and the signature
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
-line_bot_api = LineBotApi('<channel access token>')
+app = Flask(__name__)
 
-try:
-    line_bot_api.reply_message('<reply_token>', TextSendMessage(text='Hello World!'))
-except LineBotApiError as e:
-    print("error")
-    # error handle
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
+    app.run()
