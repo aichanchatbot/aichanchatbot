221b32162f0a26570207832f5f97a13128f2edc3
aichanchatbot <47549695+aichanchatbot@users.noreply.github.com>
2019-02-14 00:56:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ def docomo_api(event, docomo_res):
     # レスポンスデータから返答内容を取得
     return_data = r.json()
     docomo_res = return_data['systemText']['expression']
-    return docomo_res
+    print(docomo_res)
 
 def handle_message(event):
     line_bot_api.reply_message(
