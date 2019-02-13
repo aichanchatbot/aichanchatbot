81373ad1c6d97940fb2b88e4ecf756c61b705ea3
aichanchatbot <47549695+aichanchatbot@users.noreply.github.com>
2019-02-14 02:18:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,6 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 import os
-import requests
-import json
-from datetime import datetime
  
 app = Flask(__name__)
  
@@ -23,6 +20,7 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
  
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
 
 ## 1 ##
 ###############################################
@@ -54,55 +52,28 @@ def callback():
 
 ## 2 ##
 ###############################################
-#LINEのメッセージの取得と返信内容の設定(オウム返し)
+#LINEのメッセージの取得と返信内容の設定
 ###############################################
- 
+
+#キーワードに一致する場合はキーワードを返し、一致しない場合オウム返しする 
 #LINEでMessageEvent（普通のメッセージを送信された場合）が起こった場合に、
 #def以下の関数を実行します。
 #reply_messageの第一引数のevent.reply_tokenは、イベントの応答に用いるトークンです。 
 #第二引数には、linebot.modelsに定義されている返信用のTextSendMessageオブジェクトを渡しています。
- 
+
+talk = {
+ "こんにちは" : "こんにちは!",
+ "元気?" : "超元気です!",
+ "名前を教えて" : "私の名前はAIchanです。"
+ }
+
 @handler.add(MessageEvent, message=TextMessage)
-def docomo_api(event):
-    
-    # APIキー
-    APIKEY = "6b596f636d5262304453596f6a4d646a653643514f33446b586a57754831764462376a50427453794d5130"
-    # リクエストボディ(JSON形式)
-    send_data = {
-        "language": "ja-JP",
-        "botId": "Chatting",
-        "appId": "67552b25-9624-4fd8-8d59-6c4fc093ef9e",
-        "voiceText": "",
-        "clientData": {
-            "option": {
-                "nickname": "ポコ太",
-                "nicknameY": "ポコタ",
-                "sex": "男",
-                "age": "1",
-                "mode": "dialog"
-                },
-            },
-        "appSendTime": "YYYY-MM-DD hh:mm:ss"
-        }
-
-    # リクエストヘッダ
-    headers = {'Context-type': 'application/json'}
-    # リクエストURL
-    url = "https://api.apigw.smt.docomo.ne.jp/naturalChatting/v1/dialogue?APIKEY={}".format(APIKEY)
-    send_data['voiceText'] = "{}".format(event.message.text)
-    # 送信時間を取得
-    send_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    send_data['appSendTime'] = send_time
-    # メッセージを送信
-    r = requests.post(url, data=json.dumps(send_data), headers=headers)
-    # レスポンスデータから返答内容を取得
-    return_data = r.json()
-    docomo_res = return_data['systemText']['expression']
-
-def handle_message(event, docomo_api):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text="{}".format(docomo_res)))
+def handle_message(event):
+ if event.message.text in talk:
+     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=talk[event.message.text]))
+ 
+ else:
+  line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
     
 # ポート番号の設定
 if __name__ == "__main__":
