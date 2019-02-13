17c54bc4902dcc369585b892b0d4c30f1738d21c
aichanchatbot <47549695+aichanchatbot@users.noreply.github.com>
2019-02-13 15:31:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -68,9 +68,14 @@ talk = {
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
+    if line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=talk[event.message.text]))
+
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text))
     
 # ポート番号の設定
 if __name__ == "__main__":
