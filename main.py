29ea79f9f54a008dc6629188f33705932c34a1b9
aichanchatbot <47549695+aichanchatbot@users.noreply.github.com>
2019-02-14 14:32:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -63,12 +63,11 @@ def callback():
 #第二引数には、linebot.modelsに定義されている返信用のTextSendMessageオブジェクトを渡しています。
  
 @handler.add(MessageEvent, message=TextMessage)
-def docomo_send(event, docomo_res):
+def docomo_api(event, docomo_res):
     
     # APIキー
     APIKEY = "6b596f636d5262304453596f6a4d646a653643514f33446b586a57754831764462376a50427453794d5130"
     # リクエストボディ(JSON形式)
-    event = event.message.text
     send_data = {
         "language": "ja-JP",
         "botId": "Chatting",
@@ -90,7 +89,7 @@ def docomo_send(event, docomo_res):
     headers = {'Context-type': 'application/json'}
     # リクエストURL
     url = "https://api.apigw.smt.docomo.ne.jp/naturalChatting/v1/dialogue?APIKEY={}".format(APIKEY)
-    send_data['voiceText'] = "{}".format(event)
+    send_data['voiceText'] = "{}".format(event.message.text)
     # 送信時間を取得
     send_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     send_data['appSendTime'] = send_time
@@ -101,10 +100,10 @@ def docomo_send(event, docomo_res):
     docomo_res = return_data['systemText']['expression']
     print(docomo_res)
 
-def handle_message(event, docomo_res):
+def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=( ,docomo_res)))
+        TextSendMessage(text=docomo_api(docomo_res)))
     
 # ポート番号の設定
 if __name__ == "__main__":
