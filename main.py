84fa07f8c2db9f1bf77a425920c5ff5114dd73d1
aichanchatbot <47549695+aichanchatbot@users.noreply.github.com>
2019-02-22 22:26:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def handle_message(event):
 
     INDEX = event.message.text.find(u"名前")
     INDEX2 = event.message.text.find(u"食べたい")
-    INDEX3 = event.message.text.find(u"　")
+    INDEX3 = event.message.text.find(u" ")
 
     if INDEX != -1:
         line_bot_api.reply_message(
@@ -76,7 +76,7 @@ def handle_message(event):
     if INDEX2 != -1:
         line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text="県名　地域(駅名)　食べたい物(行きたいお店)を教えてくれたらお店を探すよ"))
+        TextSendMessage(text="県名 地域(駅名) 食べたい物(行きたいお店)を教えてくれたらお店を探すよ"))
     if INDEX3 != -1:
         gnavi_response = gnaviserch(event.message.text)
         line_bot_api.reply_message(
