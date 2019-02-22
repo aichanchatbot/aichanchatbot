777d6c55241a1516f8f37d3ad4aad711f7e24ca2
aichanchatbot <47549695+aichanchatbot@users.noreply.github.com>
2019-02-22 21:41:04 +0900

    Update line_api.py

--- line_api.py
+++ line_api.py
@@ -21,8 +21,9 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
-port = int(os.getenv("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+def port():
+ port = int(os.getenv("PORT", 5000))
+ app.run(host="0.0.0.0", port=port)
 
 ## 1 ##
 ###############################################
