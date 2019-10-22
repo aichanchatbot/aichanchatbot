e4a72b1242b985da66e0ac86da551b0e81d58eea
pocotachatbot <47549695+pocotachatbot@users.noreply.github.com>
2019-10-22 20:03:23 +0900

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,64 @@
+import json
+import sys
+import urllib.parse
+import urllib.request
+
+WEATHER_URL="http://weather.livedoor.com/forecast/webservice/json/v1?city=%s"
+CITY_CODE="230010" # AICHI(NAGOYA)
+#TODAY=0
+#TOMMOROW=1
+
+def get_weather_info():
+    try:
+        url = WEATHER_URL % CITY_CODE
+        html = urllib.request.urlopen(url)
+        html_json = json.loads(html.read().decode('utf-8'))
+    except Exception as e:
+        print ("Exception Error: ", e)
+        sys.exit(1)
+    return html_json
+
+def set_weather_info(weather_json, day):
+    min_temperature = None
+    max_temperature = None
+    try:
+        date = weather_json['forecasts'][day]['dateLabel'] #dateに変更すると日付表示可能
+        city = weather_json['location']['city']
+        weather = weather_json['forecasts'][day]['telop']
+        #max_temperature = weather_json['forecasts'][day]['temperature']['max']['celsius']
+        #min_temperature = weather_json['forecasts'][day]['temperature']['min']['celsius']
+    except TypeError:
+        # temperature data is None etc...
+        pass
+    msg = "%sの%sの天気は%sだよ。" % \
+               (date, city, weather)
+    #msg = "%s\n天気: %s\n最低気温 : %s度\n最高気温: %s度" % \
+               #(date, weather, min_temperature, max_temperature)
+    return msg
+
+def weather(info_day):
+    weather_json = get_weather_info()
+    for day in [info_day]:
+        msg = set_weather_info(weather_json, day)
+        return msg
+
+def set_weather_date(text):
+    index1 = text.find('今日')
+    index2 = text.find('明日')
+    index3 = text.find('明後日')
+
+    if index1 != -1:
+        msg = weather(0)
+        return msg
+
+    elif index2 != -1:
+        msg = weather(1)
+        return msg
+
+    elif index3 != -1:
+        msg = weather(2)
+        return msg
+
+    else:
+        msg = weather(0)
+        return msg
